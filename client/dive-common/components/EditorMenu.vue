62bcfb6b240f56073719c54c61e1b8c26f024cf0
Bryon Lewis <61746913+BryonLewis@users.noreply.github.com>
2023-08-22 15:27:00 -0400

    Fix overlay (#55)
    * installing slicer-cli
    
    * fix video overlay options rendering
    
    * reverting slicer cli
    
    * override doesn't show up unless transparency is on
    
    * revert docker compose

--- client/dive-common/components/EditorMenu.vue
+++ client/dive-common/components/EditorMenu.vue
@@ -445,6 +445,7 @@ export default Vue.extend({
                   ...overlaySettings, overrideValue: $event })"
               />
               <v-tooltip
+                v-if="overlaySettings.colorTransparency"
                 open-delay="100"
                 bottom
               >
@@ -461,11 +462,11 @@ export default Vue.extend({
               </v-tooltip>
             </v-row>
             <label
-              v-if="overlaySettings.overrideValue"
+              v-if="overlaySettings.colorTransparency && overlaySettings.overrideValue"
               for="overlay-variance"
             >Variance: {{ overlaySettings.overrideVariance }}</label>
             <input
-              v-if="overlaySettings.overrideValue"
+              v-if="overlaySettings.colorTransparency && overlaySettings.overrideValue"
               id="overlay-variance"
               type="range"
               name="overlay-variance"
@@ -478,13 +479,16 @@ export default Vue.extend({
                 ...overlaySettings, overrideVariance: Number.parseFloat($event.target.value) })"
             >
             <v-color-picker
-              v-if="overlaySettings.overrideValue"
+              v-if="overlaySettings.colorTransparency && overlaySettings.overrideValue"
               :value="overlaySettings.overrideColor || 'white'"
               hide-inputs
               @input="$emit('update:overlay-settings', {
                 ...overlaySettings, overrideColor: $event })"
             />
-            <v-row dense>
+            <v-row
+              v-if="overlaySettings.colorTransparency && overlaySettings.overrideValue"
+              dense
+            >
               <v-spacer />
               <v-tooltip
                 open-delay="100"
