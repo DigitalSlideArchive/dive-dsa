df418a6653fb023a7c9fb792a856a8677cc984a2
Bryon Lewis <Bryon.Lewis@kitware.com>
2024-05-16 12:51:51 -0400

    fix metadata model existing updates

--- server/dive_utils/metadata/models.py
+++ server/dive_utils/metadata/models.py
@@ -57,7 +57,7 @@ class DIVE_Metadata(Model):
         else:
             existing['metadata'] = metadata
             existing['filename'] = str(folder['name'])
-            existing[root] = str(root['_id'])
+            existing['root'] = str(root['_id'])
         existing = self.save(existing)
         return existing
 
