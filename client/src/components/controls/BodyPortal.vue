c14e54e2577c18b19192439ef15db3fc539cb215
Bryon Lewis <Bryon.Lewis@kitware.com>
2026-09-01 10:29:15 -0400

    update tooltip styling and  positioning

---
+++ client/src/components/controls/BodyPortal.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'BodyPortal',
+  mounted() {
+    document.body.appendChild(this.$el);
+  },
+  beforeDestroy() {
+    if (this.$el.parentNode) {
+      this.$el.parentNode.removeChild(this.$el);
+    }
+  },
+});
+</script>
+
+<template>
+  <div class="timeline-tooltip-portal">
+    <slot />
+  </div>
+</template>
